142294727c2da5da162f3630e32c605cc70b7304
Sebastian Molano <sebastian.molano@nebulae.com.co>
2020-03-22 14:50:06 -0500

    first commit

---
+++ api/emi-gateway/tools/GraphqlResponseTools.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const { of } = require('rxjs');
+const { map } = require('rxjs/operators');
+const { ApolloError } = require("apollo-server");
+const { CustomError, DefaultError } = require('./customError');
+
+const buildSuccessResponse$ = (rawRespponse) => {
+    return of(rawRespponse).pipe(
+        map(resp => {
+            return {
+                data: resp,
+                result: {
+                    code: 200
+                }
+            };
+        })
+    );
+};
+
+const buildErrorResponse$ = (errCode, rawRespponse) => {
+    return of(rawRespponse).pipe(
+        map(resp => {
+            return {
+                data: resp,
+                result: {
+                    code: errCode
+                }
+            };
+        })
+    );
+};
+
+const handleError$ = (err) => {
+    return of(err).pipe(
+        map(err => {
+            const exception = { data: null, result: {} };
+            // const isCustomError = err instanceof ApolloError;
+            const isCustomError = err instanceof CustomError;
+            if (!isCustomError) {
+                // err = new ApolloError(err.message, 1, {name: err.name, msg: err.message});
+                err = new DefaultError(err);
+            }
+            exception.result = {
+                code: err.code,
+                error: { ...err.getContent() }
+            };
+            return exception;
+        })
+    );
+}
+
+module.exports = {
+    buildSuccessResponse$, 
+    handleError$,
+    buildErrorResponse$
+}
