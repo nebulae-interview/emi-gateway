5bf1300617a4fa96f49a8b784ed00cbd401746e0
Sebastian Molano <sebastian.molano@nebulae.com.co>
2020-03-22 15:11:10 -0500

    APOLLO_ENGINE_API_KEY is now optional

--- api/emi-gateway/server.js
+++ api/emi-gateway/server.js
@@ -3,8 +3,8 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').load();
 }
- 
-const { map, catchError} = require('rxjs/operators');
+
+const { map, catchError } = require('rxjs/operators');
 const { of } = require('rxjs');
 
 // const graphqlServer = require('apollo-server-express');
@@ -57,7 +57,7 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 //Express Server
 const app = express();
 
-app.use(bodyParser.json({limit: '4mb'}));
+app.use(bodyParser.json({ limit: '4mb' }));
 
 const jwtPublicKey = process.env.JWT_PUBLIC_KEY.replace(/\\n/g, '\n');
 
@@ -71,7 +71,7 @@ app.use(process.env.GRAPHQL_HTTP_END_POINT, expressJwt({
 
 // Defines GET request that is going to be use by kubelet to identify if the gateway is HEALTHY
 app.get(process.env.GRAPHQL_LIVENESS_HTTP_END_POINT, function (req, res) {
-    res.sendStatus(200) 
+    res.sendStatus(200)
 });
 
 // bodyParser is needed just for POST.
@@ -81,7 +81,7 @@ app.use(cors());
 const server = new ApolloServer({
     schema,
     context: ({ req, connection }) => {
-        if(connection){
+        if (connection) {
             // check connection for metadata
             return connection.context;
         } else {
@@ -91,32 +91,32 @@ const server = new ApolloServer({
                 broker,
             });
         }
-	},
+    },
     introspection: true,
     //playground: true,
     playground: {
-      endpoint: process.env.GRAPHIQL_HTTP_END_POINT,
-      subscriptionEndpoint: `ws://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHQL_WS_END_POINT}`,
-      settings: {
-        'editor.theme': 'dark'
-      }
+        endpoint: process.env.GRAPHIQL_HTTP_END_POINT,
+        subscriptionEndpoint: `ws://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHQL_WS_END_POINT}`,
+        settings: {
+            'editor.theme': 'dark'
+        }
     },
     subscriptions: {
         path: process.env.GRAPHQL_WS_END_POINT,
-        onConnect: async (connectionParams, webSocket, connectionContext) => {           
+        onConnect: async (connectionParams, webSocket, connectionContext) => {
             console.log(`GraphQL_WS.onConnect: origin=${connectionContext.request.headers.origin} url=${connectionContext.request.url}`);
             const encondedToken$ = connectionParams.authToken
                 ? of(connectionParams.authToken)
                 : connectionContext.request.headers['authorization']
                     ? of(connectionContext.request.headers['authorization'])
-                        .pipe( map(header => header.replace('Bearer ', '')) )
+                        .pipe(map(header => header.replace('Bearer ', '')))
                     : undefined;
             if (!encondedToken$) {
                 throw new Error('Missing auth token!');
             }
             //this is the default action to do when unsuscribing                
             const authToken = await encondedToken$.pipe(
-                map(encondedToken => jsonwebtoken.verify(encondedToken, jwtPublicKey,{ algorithms: ['RS256']}))
+                map(encondedToken => jsonwebtoken.verify(encondedToken, jwtPublicKey, { algorithms: ['RS256'] }))
             )
                 .toPromise()
                 .catch(error => console.error(`Failed to verify jwt token on WebSocket channel: ${error.message}`, error));
@@ -125,22 +125,22 @@ const server = new ApolloServer({
             return { broker, authToken, encondedToken, webSocket };
         },
         onDisconnect: (webSocket, connectionContext) => {
-            if(webSocket.onUnSubscribe){
+            if (webSocket.onUnSubscribe) {
                 webSocket.onUnSubscribe.subscribe(
-                    (evt) => console.log(`webSocket.onUnSubscribe: ${JSON.stringify({evt})};  origin=${connectionContext.request.headers.origin} url=${connectionContext.request.url};`),
+                    (evt) => console.log(`webSocket.onUnSubscribe: ${JSON.stringify({ evt })};  origin=${connectionContext.request.headers.origin} url=${connectionContext.request.url};`),
                     error => console.error(`GraphQL_WS.onDisconnect + onUnSubscribe; origin=${connectionContext.request.headers.origin} url=${connectionContext.request.url}; Error: ${error.message}`, error),
                     () => console.log(`GraphQL_WS.onDisconnect + onUnSubscribe: Completed OK; origin=${connectionContext.request.headers.origin} url=${connectionContext.request.url};`)
                 );
-            }else{
+            } else {
                 console.log(`GraphQL_WS.onDisconnect; origin=${connectionContext.request.headers.origin} url=${connectionContext.request.url}; WARN: no onUnSubscribe callback found`);
-            }                
+            }
         },
     },
     tracing: true,
     cacheControl: true
-  });
+});
 
-  server.applyMiddleware({ app, path: process.env.GRAPHQL_HTTP_END_POINT });
+server.applyMiddleware({ app, path: process.env.GRAPHQL_HTTP_END_POINT });
 
 // ApolloEngine
 const { ApolloEngine } = require('apollo-engine');
@@ -148,12 +148,13 @@ const { ApolloEngine } = require('apollo-engine');
 // Initialize engine with your API key. Alternatively,
 // set the ENGINE_API_KEY environment variable when you
 // run your program.
-const engine = new ApolloEngine({
-    apiKey: process.env.APOLLO_ENGINE_API_KEY,
+const engineOps = {
     logging: {
         level: process.env.APOLLO_ENGINE_LOG_LEVEL // opts: DEBUG, INFO (default), WARN or ERROR.
     },
-});
+};
+if (process.env.APOLLO_ENGINE_API_KEY) engineOps.apiKey = process.env.APOLLO_ENGINE_API_KEY;
+const engine = new ApolloEngine(engineOps);
 
 // Wrap the Express server and combined with WebSockets
 const ws = http.createServer(app);
@@ -161,22 +162,22 @@ server.installSubscriptionHandlers(ws);
 
 engine.listen({
     port: PORT,
-    httpServer: ws,  
+    httpServer: ws,
     //expressApp: app,
     graphqlPaths: [
         process.env.GRAPHQL_HTTP_END_POINT
-        ,process.env.GRAPHIQL_HTTP_END_POINT
+        , process.env.GRAPHIQL_HTTP_END_POINT
     ],
 },
- () => {
-    console.log(`Apollo Server is now running on http://localhost:${PORT}`);
-    console.log(`HTTP END POINT: http://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHQL_HTTP_END_POINT}`);
-    console.log(`WEBSOCKET END POINT: ws://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHQL_WS_END_POINT}`);
-    console.log(`GRAPHIQL PAGE: http://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHIQL_HTTP_END_POINT}`);
-});
+    () => {
+        console.log(`Apollo Server is now running on http://localhost:${PORT}`);
+        console.log(`HTTP END POINT: http://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHQL_HTTP_END_POINT}`);
+        console.log(`WEBSOCKET END POINT: ws://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHQL_WS_END_POINT}`);
+        console.log(`GRAPHIQL PAGE: http://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHIQL_HTTP_END_POINT}`);
+    });
 
 engine.on('error', (err) => {
     console.log('There was an error starting the server or Engine.');
     console.error(err);
     process.exit(1);
-  });
+});
